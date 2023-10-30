9d338e4fdb0151e787ead8d71b45fed37127275b
EFFemole <go.netgle@gmail.com>
2023-10-30 13:08:10 +0200

    update

---
+++ bookstore-frontend/src/hooks/useCurrentRouteNumber.ts
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export const useCurrentRouteNumber = () => {
+  function getRouteNumber(pathname: string): number {
+    const cleanPathname = pathname.replace("/", "");
+
+    if (cleanPathname === "bookcatalog") {
+      return 0;
+    } else if (cleanPathname === "library") {
+      return 1;
+    } else if (cleanPathname === "settings") {
+      return 2;
+    }
+
+    return 0;
+  }
+
+  const location = useLocation();
+
+  const [selectedTab, setSelectedTab] = useState<number>(
+    getRouteNumber(location.pathname)
+  );
+
+  useEffect(() => {
+    setSelectedTab(getRouteNumber(location.pathname));
+  }, [location]);
+
+  return selectedTab;
+};
